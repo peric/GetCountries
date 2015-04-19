7df26ba4fe5409cd22077e8cde9ad369fea742b1
drazen <peric.drazhen@gmail.com>
2015-04-19 23:18:21 +0200

    Started to reactify

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// Options
+    // Columns
+        // select
+    // Additional options
+        // select
+    // Output type
+        // select
+
+// Generate
+// Output
+
+var options = [
+//    { name: 'Web Development', price: 300 },
+//    { name: 'Design', price: 400 },
+//    { name: 'Integration', price: 250 },
+//    { name: 'Training', price: 220 }
+];
+
+var additionalOptions = [
+    { }
+];
+
+var outputTypes = [
+    { }
+];
+
+var GeneratorApp = React.createClass({
+    getInitialState: function() {
+        // TODO: return
+    },
+
+    render: function() {
+        // TODO: define additional function
+        // TODO: return something that calls additional function
+
+        // TODO: return options and result
+
+        // TODO: return them into container
+    }
+});
+
+React.renderComponent(
+    <GeneratorApp options= { options } />,
+    document.body
+);
+
+//var GeneratorOptions = React.createClass({
+//    render: function() {
+//
+//    }
+//});
